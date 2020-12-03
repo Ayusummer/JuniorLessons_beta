8413df89bf95acccb1f8c6e4448c96b85f5ab1fa
Ayusummer <1369661643@qq.com>
2020-12-03 21:20:46 +0800

    上机6_递归

---
+++ CPlusPlus/exerciseSave/上机6/二分法求零点/dichotomy.cpp
@@ -0,0 +1,39 @@
+﻿// f(x) = x**3 + x**2 - 3x - 3 = (x + 1)(x**2 - 3)
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+double f(double x) {
+	return x * x * x + x * x - 3 * x - 3;
+}
+
+double zeros_correctInput(double a, double b) {
+	double x = (a + b) / 2;
+	if (f(x) == 0 or fabs(a - b) < pow(10, -6))
+		return x;
+	else if (f(x) * f(a) < 0)
+		return zeros_correctInput(a, x);
+	else
+		return zeros_correctInput(x, b);
+}
+
+
+
+int main() {
+	int a, b;
+	cout << "请输入方程 x**3 + x**2 - 3x - 3 含零点的一个区间的左右端点(中间用空格隔开): ";
+	while (true) {
+		cin >> a >> b;
+		if (f(a) * f(b) < 0) {
+			cout << "该方程的一个解为:" << zeros_correctInput(a, b) << endl;
+			break;
+		}
+		else {
+			cout << "给入区间内无法确定有无零点,请重新输入区间左右端点(Ctrl+C以退出):";
+		}
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ CPlusPlus/exerciseSave/上机6/递归法求逆序数/递归算法输出倒序数.cpp
@@ -0,0 +1,31 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+
+int reverseNum_assist(int x, int x_reverse) {
+	if (x != 0) {
+		x_reverse = x_reverse * 10 + x % 10;
+		x = x / 10;
+		reverseNum_assist(x, x_reverse);
+	}
+	else
+		return x_reverse;
+}
+
+
+int reverseNum(int x) {
+	int	x_reverse = x % 10;	// 取出x的最低位
+	x = x / 10;				// x去掉最低位
+	return reverseNum_assist(x, x_reverse);
+}
+
+
+int main()
+{
+	int x;
+	cout << "请输入一个整数: ";
+	cin >> x;
+	cout << "该数的逆序数为:" << reverseNum(x) << endl;
+	system("pause");
+	return 0;
+}
